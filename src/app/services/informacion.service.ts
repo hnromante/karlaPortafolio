503d5386da0e405c10017e52484df4ce4780e17c
Hugo Nuñez Romante <hugonromante@outlook.com>
2018-03-26 22:58:30 -0300

    Servicio de sobreMi firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,16 +4,31 @@ import {Http} from "@angular/http";
 @Injectable()
 export class InformacionService {
   info:any = {};
-  cargada:boolean = false;
+  cargadaInfoPag:boolean = false;
+  cargadaSobreMi:boolean = false;
+  sobreMi:any[] =  [];
+
   constructor(public http:Http) { 
+
+      this.cargarInfo();
+      this.cargarSobreMi();
+  } 
+
+  public cargarInfo(){
     this.http.get("assets/data/info.pagina.json").
               subscribe(data => { 
-                console.log(data.json());
                 this.info = data.json();
-                this.cargada = true;
+                this.cargadaInfoPag = true;
 
               })
-
   }
+  public cargarSobreMi(){
+    this.http.get("https://karlafolio.firebaseio.com/sobremi.json").
+              subscribe(data => { 
+                this.sobreMi = data.json();
+                console.log(this.sobreMi)
+                this.cargadaSobreMi = true;
 
+              })
+  }
 }
